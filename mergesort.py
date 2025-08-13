2f34455e1c35aa83616343ed146b09c4041623c5
Asma Junaid <asmajunaid@Mac.home>
2025-08-13 15:48:07 +0200

    Add mergesort script

---
+++ mergesort.py
@@ -0,0 +1,23 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+input_list = input("Enter numbers, separated by ',': ").split(',')
+value_list = [int(x.strip()) for x in input_list]
+sorted_array = merge_sort(value_list)
+print(sorted_array)
